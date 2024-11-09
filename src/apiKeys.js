6a4f8db4af7372210277dcc0c0a591cbde769eec
Gunjan Wanjare <gunjanwanjare6@gmail.com>
2024-11-10 01:50:04 +0530

    initial commit

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "bc54d59b5841159826ad4137f0030230",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
